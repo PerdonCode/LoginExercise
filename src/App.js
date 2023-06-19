921c503b8b094a55df1fee4c1d4934dcf79b9e85
PerdonCode <niels.perdon1@gmail.com>
2023-06-19 15:14:28 +0200

    Added useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
@@ -7,13 +7,40 @@ import MainHeader from './components/MainHeader/MainHeader';
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  // this is not the right way of doing this (big advantage: infinite loop)
+  // const storedUsedLoggedInformation = localStorage.getItem("isLoggedIn");
+  // bc if we reassign a state the function runs again...
+  // so after setIsloggedIn is called the function runs agains and we get an infinite loop 
+  // if(storedUsedLoggedInformation ==='1'){
+  //   setIsLoggedIn(true);
+  // }
+
+
+  // useffect will run after the whole fuction has run and only if the dependencie had changed
+  useEffect(() => {
+    const storedUsedLoggedInformation = localStorage.getItem("isLoggedIn");
+     
+    if(storedUsedLoggedInformation ==='1'){
+      setIsLoggedIn(true);
+    }
+  },[]);
+
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+
+    // set user to is loged in we dont need useEffect here...
+    // but IF the user retreshed te page or leaves and comes back later
+    // the user login session is gone and needs to log in again
+    // this comes beacause if u use a useState the function is called again and this time 
+    // is has nog user login credentials 
+    // here comes useEffect in play!
+    localStorage.setItem('isLoggedIn', '1');
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn');
     setIsLoggedIn(false);
   };
 
